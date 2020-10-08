cfca28c4f82ee5eca91acfae376baef2bcb3faff
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-10-09 00:17:08 +0300

    hw done

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import imageRef from "./gallery-items.js";
 
-const galleryContainer = document.querySelector(".js-gallery");
+const refs = {
+  openModal: document.querySelector("div.lightbox"),
+  closeModal: document.querySelector("button.lightbox__button"),
+  galleryContainer: document.querySelector(".js-gallery"),
+  modalOpenGalleryItem: document.querySelector(".lightbox__image"),
+  lightboxOverlay: document.querySelector(".lightbox__overlay"),
+};
+console.log(refs.modalOpenGalleryItem);
 const galleryMarkap = createGalleryCardsMarkup(imageRef);
-//console.log(createGalleryCardsMarkup(imageRef));
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkap);
+
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryMarkap);
 
 function createGalleryCardsMarkup(imageRef) {
   return imageRef
@@ -12,8 +19,8 @@ function createGalleryCardsMarkup(imageRef) {
       <li class="gallery__item">
         <a
           class="gallery__link"
-          href="${original}"
-        >
+          
+          >
           <img
             class="gallery__image"
             src="${preview}"
@@ -24,4 +31,52 @@ function createGalleryCardsMarkup(imageRef) {
       </li>`;
     })
     .join(``);
+}
+//href="${original}"
+refs.galleryContainer.addEventListener("click", onOpenModal);
+refs.closeModal.addEventListener("click", onCloseModal);
+refs.lightboxOverlay.addEventListener("click", onOverlayClick);
+
+//refs.galleryContainer.removeAttribute("href");
+
+function onOpenModal(evt) {
+  window.addEventListener("keydown", onEscKeyPress);
+  refs.openModal.classList.add("is-open");
+  console.log(`Модалка открыта`);
+  //console.log(evt.target);
+
+  refs.modalOpenGalleryItem.setAttribute(
+    "src",
+    `${evt.target.getAttribute("data-source")}`
+  );
+  refs.modalOpenGalleryItem.setAttribute(
+    "alt",
+    `${evt.target.getAttribute("alt")}`
+  );
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  refs.openModal.classList.remove("is-open");
+  console.log(`Модалка закрыта`);
+  refs.modalOpenGalleryItem.setAttribute("src", ``);
+  refs.modalOpenGalleryItem.setAttribute("alt", ``);
+}
+
+function onOverlayClick(evt) {
+  //console.log(evt.currentTarget);
+  //console.log(evt.target);
+
+  if (evt.currentTarget === evt.target) {
+    console.log(`Клик по бекдропу`);
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(evt) {
+  console.log(evt.code);
+
+  if (evt.code === "Escape") {
+    onCloseModal();
+  }
 }
